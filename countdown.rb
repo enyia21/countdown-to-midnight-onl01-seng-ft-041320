282fca13af9e874a6f191743ff01a870ce49c7cb
Chibuisi Enyia <enyia21@gmail.com>
2020-04-16 14:33:21 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,14 +2,14 @@
 
 def countdown(number)
   while number>=0
-    puts "#{number}"
+    puts "#{number} SECOND(S)!"
     number-=1
   end
 end
 
 def countdown_with_sleep(number)
   while number>=0
-    puts "#{number}"
+    puts "#{number} SECOND(S)!"
     number-=1
     sleep 1
   end
